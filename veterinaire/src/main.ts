c7330db6467a0418bcb18676232ca1680b626851
shaymabassou <chaimabassou167@gmail.com>
2025-01-12 16:19:27 +0100

    gg

--- veterinaire/src/main.ts
+++ veterinaire/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // Remplacez par l'URL de votre frontend
+    origin: '*', // Remplacez par l'URL de votre frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
